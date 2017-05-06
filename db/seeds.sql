fc8e628c025f92d9f7a5f586c72b0370fe86c47d
Marissa Wyss <mwyss1@gmail.com>
2017-05-05 20:19:19 -0700

    Add files to repository

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mysql = require('mysql');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({	extended: false }));
+
+app.use(methodOverride('_method'));
+
+// Serve static content for the app from the "public" directory in the application directory.
+//app.use(express.static(process.cwd() + "/public"));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+var routes = require("./controllers/burgers_controller.js");
+
+var port = 3000;
+
+app.use("/", routes);
+
+app.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
+
